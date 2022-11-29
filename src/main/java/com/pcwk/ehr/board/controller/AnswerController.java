4d360ecf40dc1a9e9b1f0fb484285556e6f9cf1f
ITSC <ITSC@DESKTOP-ONE4R81>
2022-11-29 17:34:07 +0900

    controller 삭제

--- src/main/java/com/pcwk/ehr/board/controller/AnswerController.java
+++ src/main/java/com/pcwk/ehr/board/controller/AnswerController.java
@@ -7,6 +7,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,6 +19,7 @@ import com.pcwk.ehr.board.domain.AnswerVO;
 import com.pcwk.ehr.board.service.AnswerService;
 import com.pcwk.ehr.cmn.Message;
 
+//@Controller("answerController")
 @RequestMapping("answer")
 public class AnswerController {
 	
