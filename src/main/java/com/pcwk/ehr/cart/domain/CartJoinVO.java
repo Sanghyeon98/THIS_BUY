31b4fcd9514d538973c9014079b5ce08ec2955ab
S.H.Park <ppsh0703@daum.net>
2022-12-02 17:08:06 +0900

    카트조인 VO

---
+++ src/main/java/com/pcwk/ehr/cart/domain/CartJoinVO.java
@@ -0,0 +1,113 @@
+package com.pcwk.ehr.cart.domain;
+
+import com.pcwk.ehr.cmn.DTO;
+
+public class CartJoinVO extends DTO {
+
+	private int         cartNO;   				// 장바구니 번호
+	private int         itemNO;   				// 상품 번호
+	private String      memberId; 			    // 사용자 아이디
+	private int         quantity; 				// 상품 수량
+	private  String		orgName;                //원본파일명
+	private  String		saveName;               //저장파일명
+	private  String		path;                   //경로
+	private int 		imageNo;		        // 이미지 번호
+	private String      name;		            // 상품명
+	private int         price;			        // 가격_default 0
+	private int         finalPrice;	            // 최종가격_default 0
+	public CartJoinVO () {}
+	public CartJoinVO(int cartNO, int itemNO, String memberId, int quantity, String orgName, String saveName,
+			String path, int imageNo, String name, int price, int finalPrice) {
+		super();
+		this.cartNO = cartNO;
+		this.itemNO = itemNO;
+		this.memberId = memberId;
+		this.quantity = quantity;
+		this.orgName = orgName;
+		this.saveName = saveName;
+		this.path = path;
+		this.imageNo = imageNo;
+		this.name = name;
+		this.price = price;
+		this.finalPrice = finalPrice;
+	}
+	public int getCartNO() {
+		return cartNO;
+	}
+	public void setCartNO(int cartNO) {
+		this.cartNO = cartNO;
+	}
+	public int getItemNO() {
+		return itemNO;
+	}
+	public void setItemNO(int itemNO) {
+		this.itemNO = itemNO;
+	}
+	public String getMemberId() {
+		return memberId;
+	}
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public String getOrgName() {
+		return orgName;
+	}
+	public void setOrgName(String orgName) {
+		this.orgName = orgName;
+	}
+	public String getSaveName() {
+		return saveName;
+	}
+	public void setSaveName(String saveName) {
+		this.saveName = saveName;
+	}
+	public String getPath() {
+		return path;
+	}
+	public void setPath(String path) {
+		this.path = path;
+	}
+	public int getImageNo() {
+		return imageNo;
+	}
+	public void setImageNo(int imageNo) {
+		this.imageNo = imageNo;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public int getFinalPrice() {
+		return finalPrice;
+	}
+	public void setFinalPrice(int finalPrice) {
+		this.finalPrice = finalPrice;
+	}
+	@Override
+	public String toString() {
+		return "CartJoinVO [cartNO=" + cartNO + ", itemNO=" + itemNO + ", memberId=" + memberId + ", quantity="
+				+ quantity + ", orgName=" + orgName + ", saveName=" + saveName + ", path=" + path + ", imageNo="
+				+ imageNo + ", name=" + name + ", price=" + price + ", finalPrice=" + finalPrice + ", toString()="
+				+ super.toString() + "]";
+	}
+
+	
+	
+	
+	
+	
+}
