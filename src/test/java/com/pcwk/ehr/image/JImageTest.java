789d865c8f26ad139f7c0730ce8fdc145f80af91
S.H.Park <ppsh0703@daum.net>
2022-12-02 14:23:05 +0900

    이미지 테스트 커밋

---
+++ src/test/java/com/pcwk/ehr/image/JImageTest.java
@@ -0,0 +1,52 @@
+package com.pcwk.ehr.image;
+
+import static org.junit.Assert.fail;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.pcwk.ehr.image.dao.ImageDao;
+import com.pcwk.ehr.image.domain.ImageVO;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/root-context.xml"
+								 ,"file:src/main/webapp/WEB-INF/spring/appServlet/servlet-context.xml"})
+public class JImageTest {
+	final Logger LOG = LogManager.getLogger(getClass());
+	@Autowired //테스트 오브젝트가 만들어지고 나면 스프링 테스트 컨텍스트에 의해 자동으로 값으 주입된다.
+	ApplicationContext context;
+	
+	@Autowired
+	ImageDao dao;
+	
+	ImageVO   imageVO1;
+	ImageVO   imageVO2;
+	ImageVO   imageVO3;
+	
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {                                                               
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
