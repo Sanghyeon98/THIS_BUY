c90a94f9cac81d61d8572ee070fd88aa52a5de83
ITSC <ITSC@DESKTOP-ONE4R81>
2022-12-05 17:52:27 +0900

    controller 복구완료

--- src/main/java/com/pcwk/ehr/board/controller/BoardController.java
+++ src/main/java/com/pcwk/ehr/board/controller/BoardController.java
@@ -6,6 +6,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -15,56 +16,128 @@ import com.pcwk.ehr.board.domain.BoardVO;
 import com.pcwk.ehr.board.service.BoardService;
 import com.pcwk.ehr.cmn.Message;
 
-//@Controller
+
+@Controller("boardController")
+@RequestMapping("board")
 public class BoardController {
 	final Logger LOG = LogManager.getLogger(getClass());
 
 	@Autowired
 	BoardService boardService;
 
-	// 화면 파일
-	final String VIEW_NAME = "board/board_mng";
-
-	public BoardController() {
-	}
-
-	// 화면
-	@RequestMapping(value = "/view.do")
-	public String view() {
-		LOG.debug("┌=============================┐");
-		LOG.debug("│view=                        │");
-		LOG.debug("└=============================┘");
-		/*
-		 * prefix = /WEB-INF/views/ user/user_mng ==> /WEB-INF/views/user/user_mng.jsp
-		 * suffix = .jsp
-		 */
-		return VIEW_NAME;
-	}
+	public BoardController() {}
 	
-	@RequestMapping(value = "/doDelete.do", method = RequestMethod.GET
+	@RequestMapping(value = "/doUpdate.do",method=RequestMethod.GET
 			,produces = "application/json;charset=UTF-8")
-	public String doDelete(BoardVO inVO)throws SQLException {
+	@ResponseBody //비동기 처리를 하는 경우, HTTP 요청 부분의 body부분이 그대로 브라우저에 전달된다.	
+	public String doUpdate(BoardVO inVO)throws SQLException{
 		String jsonString = "";
 		LOG.debug("┌=============================┐");	
-		LOG.debug("|inVO="+inVO);	
+		LOG.debug("|inVO="+inVO);
+		int flag = boardService.doUpdate(inVO);
 		
-		int flag = boardService.doDelete(inVO);
-		
-		LOG.debug("|flag="+flag);
-		
-		String message = "";
+		String message = "";//json으로 전달할 메시지
 		if(1==flag) {
-			message = inVO.getTitle()+"가 삭제 되었습니다.";
+			message = inVO.getTitle()+"수정 되었습니다.";
 		}else {
-			message = inVO.getTitle()+" 삭제 실패!";
+			message = inVO.getTitle()+"수정 실패";
 		}
 		
-		jsonString = new Gson().toJson(new Message(String.valueOf(flag),message));
+		Message messageVO=new Message(String.valueOf(flag), message);
+		
+		jsonString = new Gson().toJson(messageVO);
+		
 		LOG.debug("|jsonString="+jsonString);
-		LOG.debug("└=============================┘");	
+		LOG.debug("└=============================┘");			
+		return 	jsonString;		
+	}
+	
+	/**
+	 * 검색
+	 * @param inVO
+	 * @param model
+	 * @return
+	 * @throws SQLException
+	 */
+	@RequestMapping(value = "/doSelectOne.do", method = RequestMethod.GET)
+	public String doSelectOne(BoardVO inVO, Model model)throws SQLException {
+		String jsonString = "";
+		LOG.debug("┌=============================┐");	
+		LOG.debug("|inVO="+inVO);
+		LOG.debug("└=============================┘");
+		
+		
+		BoardVO outVO = boardService.doSelectOne(inVO);
+		
+		jsonString = new Gson().toJson(outVO);
+		LOG.debug("┌=============================┐");	
+		LOG.debug("|outVO="+outVO);
+		model.addAttribute("vo", outVO);
+		LOG.debug("└=============================┘");
 		
 		return jsonString;
 	}
-	
-	
+
+	/**
+	 * 추가
+	 * 
+	 * @param inVO
+	 * @return
+	 * @throws SQLException
+	 */
+	@RequestMapping(value = "/doSave.do", method = RequestMethod.GET, produces = "application/json;charset=UTF-8")
+	@ResponseBody
+	public String doSave(BoardVO inVO) throws SQLException {
+		String jsonString = "";
+		LOG.debug("┌=============================┐");
+
+		LOG.debug("|inVO=" + inVO);
+
+		int flag = this.boardService.doSave(inVO);
+
+		String message = "";
+		if (1 == flag) {
+			message = inVO.getTitle() + "이 등록되었습니다.";
+		} else {
+			message = inVO.getTitle() + "등록 실패!";
+		}
+		LOG.debug("|flag=" + flag);
+		jsonString = new Gson().toJson(new Message(flag + "", message));
+		LOG.debug("|jsonString=" + jsonString);
+
+		return jsonString;
+	}
+
+	/**
+	 * 삭제
+	 * 
+	 * @param inVO
+	 * @return
+	 * @throws SQLException
+	 */
+	@RequestMapping(value = "/doDelete.do", method = RequestMethod.GET, produces = "application/json;charset=UTF-8")
+	@ResponseBody
+	public String doDelete(BoardVO inVO) throws SQLException {
+		String jsonString = "";
+		LOG.debug("┌=============================┐");
+		LOG.debug("|inVO=" + inVO);
+
+		int flag = boardService.doDelete(inVO);
+
+		LOG.debug("|flag=" + flag);
+
+		String message = "";
+		if (1 == flag) {
+			message = "삭제 되었습니다.";
+		} else {
+			message = "삭제 실패!";
+		}
+
+		jsonString = new Gson().toJson(new Message(String.valueOf(flag), message));
+		LOG.debug("|jsonString=" + jsonString);
+		LOG.debug("└=============================┘");
+
+		return jsonString;
+	}
+
 }
