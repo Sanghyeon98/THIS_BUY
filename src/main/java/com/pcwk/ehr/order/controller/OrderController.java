0bfaf3f7b12717bb9a1c968da21b871c0593affe
KmSeo <hoddhdl@naver.com>
2022-12-16 16:50:42 +0900

    어노테이션

--- src/main/java/com/pcwk/ehr/order/controller/OrderController.java
+++ src/main/java/com/pcwk/ehr/order/controller/OrderController.java
@@ -1,11 +1,13 @@
 package com.pcwk.ehr.order.controller;
 
+
 import java.sql.SQLException;
 import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,7 +19,8 @@ import com.pcwk.ehr.cmn.SearchVO;
 import com.pcwk.ehr.code.service.CodeService;
 import com.pcwk.ehr.order.domain.OrderVO;
 import com.pcwk.ehr.order.service.OrderService;
-
+@Controller("orderController")
+@RequestMapping("order")
 public class OrderController {
 	final Logger LOG = LogManager.getLogger(getClass());
 	
